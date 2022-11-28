ab50a134421947a395ffd64a670c4222008899ce
linkedbyte <linkedbyte@126.com>
2022-11-28 21:53:03 +0800

    Update web_server.py

--- client/web_server.py
+++ client/web_server.py
@@ -9,6 +9,7 @@ import socketserver
 from http import HTTPStatus
 from functools import partial
 import shutil
+import json
 
 class BitTunnelHTTPReqeustHandler(BaseHTTPRequestHandler):
     server_version = "BitTunnel-HTTP/1.0"
@@ -17,8 +18,10 @@ class BitTunnelHTTPReqeustHandler(BaseHTTPRequestHandler):
         '.css': 'text/css',
         '.js': 'application/javascript',
         '.jpeg': 'image/jpeg',
-        '.jpg': 'image/jpeg',
+        '.jpg': 'image/jpeg'
     }
+    default_content_type = "application/json"
+    default_res_content = "{'status':%(code)d,'message':'%(message)s','data':'%(data)s'}"
 
     def __init__(self, *args, directory = None,**kwargs):
         if directory is None:
@@ -27,41 +30,19 @@ class BitTunnelHTTPReqeustHandler(BaseHTTPRequestHandler):
         super().__init__(*args, **kwargs)
 
     def do_GET(self):
-        f = self.send_head()
-        if f:
-            try:
-                shutil.copyfileobj(f, self.wfile)
-            finally:
-                f.close()
-    
-    def do_POST(self):
-        length = self.headers.get('content-length')
-        try:
-            nbytes = int(length)
-        except (TypeError, ValueError):
-            nbytes = 0
-        if nbytes > 0:
-            data = self.rfile.read(nbytes)
-        else:
-            data = None
-            
-        pass
-    
-    def send_head(self):
         path = self.translate_path(self.path)
-        f = None
+        local_file = None
         ctype = self.guess_type(path)
         if path.endswith("/"):
             self.send_error(HTTPStatus.NOT_FOUND, "File not found")
             return None
         try:
-            f = open(path, 'rb')
+            local_file = open(path, 'rb')
         except OSError:
             self.send_error(HTTPStatus.NOT_FOUND, "File not found")
             return None
-
         try:
-            fs = os.fstat(f.fileno())
+            fs = os.fstat(local_file.fileno())
             if ("If-Modified-Since" in self.headers
                     and "If-None-Match" not in self.headers):
                 try:
@@ -80,19 +61,50 @@ class BitTunnelHTTPReqeustHandler(BaseHTTPRequestHandler):
                         if last_modif <= ims:
                             self.send_response(HTTPStatus.NOT_MODIFIED)
                             self.end_headers()
-                            f.close()
+                            local_file.close()
                             return None
-
             self.send_response(HTTPStatus.OK)
             self.send_header("Content-type", ctype)
             self.send_header("Content-Length", str(fs[6]))
-            self.send_header("Last-Modified",
-                self.date_time_string(fs.st_mtime))
+            self.send_header("Last-Modified",self.date_time_string(fs.st_mtime))
             self.end_headers()
-            return f
+            shutil.copyfileobj(local_file, self.wfile)
         except:
-            f.close()
+            local_file.close()
             raise
+    
+    def do_POST(self):
+        length = self.headers.get('content-length')
+        try:
+            nbytes = int(length)
+        except (TypeError, ValueError):
+            nbytes = 0
+        if nbytes > 0:
+            data = self.rfile.read(nbytes)
+            self.serve_command(data)
+        else:
+            self.send_error(HTTPStatus.BAD_REQUEST,'Invalid post')
+
+    def serve_command(self,post_content):
+        body = self.default_res_content % {'code':500,'message':'json format error','data':''}
+        try:
+            params = json.loads(post_content)
+            command = params.get('command',None)
+            match command:
+                case 'status':
+                    pass
+                case _:
+                    pass
+        except Exception as ex:
+            pass
+        
+        body = body.encode('UTF-8', 'replace')
+        self.send_response(HTTPStatus.OK)
+        self.send_header('Connection', 'close')
+        self.send_header("Content-Type", self.default_content_type)
+        self.send_header('Content-Length', str(len(body)))
+        self.end_headers()
+        self.wfile.write(body)
 
     def guess_type(self, path):
         base, ext = posixpath.splitext(path)
